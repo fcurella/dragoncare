4cb069cb359988a3bf29955c414ecdfcd878dda8
Flavio Curella <flavio.curella@gmail.com>
2022-06-15 14:02:39 -0500

    eggs can hatch

--- source/models/Dragon.lua
+++ source/models/Dragon.lua
@@ -1,5 +1,10 @@
 import "CoreLibs/object"
 import "NamedEntity"
 
-class('Dragon').extends(NamedEntity)
+class('Dragon', {name = "", age = 0}).extends(NamedEntity)
+function Dragon:init(name, age)
+    Dragon.super.init(self, name)
+    self.age = age
+end
+
 function Dragon:feed(food) self.hunger = self.hunger - food.nutritionalValue end
--- source/models/Egg.lua
+++ source/models/Egg.lua
@@ -1,7 +1,6 @@
 import "CoreLibs/object"
 import "NamedEntity"
+import "Dragon"
 
 class('Egg').extends(NamedEntity)
-function Egg:hatch()
-    -- TODO
-end
+function Egg:hatch() return Dragon(self.name) end
