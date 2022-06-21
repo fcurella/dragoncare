7d42033093e0c81ef6df25fb07d4980ca7cf7fbe
Flavio Curella <flavio.curella@gmail.com>
2022-06-21 14:36:29 -0500

    update timers in loop

--- source/main.lua
+++ source/main.lua
@@ -1,3 +1,7 @@
+import "CoreLibs/timer"
 import "gameState"
 
-function playdate.update() gameState.router:current():update(gameState) end
+function playdate.update()
+    gameState.router:current():update(gameState)
+    playdate.timer.updateTimers()
+end
